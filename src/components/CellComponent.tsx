aea8013427998e194408902862ff68988f40af63
Dmytro <dimayuda@gmail.com>
2023-01-31 17:51:37 +0200

    third

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Cell} from "../models/Cell";
+
+interface CellProps {
+    cell: Cell;
+}
+
+const CellComponent = () => {
+    return (
+        <div
+            className="cell"
+        >
+            
+        </div>
+    );
+};
+
+export default CellComponent;
